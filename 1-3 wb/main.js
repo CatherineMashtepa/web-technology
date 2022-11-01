90eac849479092a3d7c6d2697546205fe25f8ba9
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-11-01 13:51:31 +0300

    fufu

---
+++ 1-3 wb/main.js
@@ -0,0 +1,14 @@
+function darkmode () {
+    const body = document.body
+    const wasDarkMode = localStorage.getItem('darkmode') === 'true'
+
+    localStorage.setItem('darkmode',!wasDarkMode)
+    body.classList.toggle('dark-mode',!wasDarkMode)
+}
+
+document.querySelector('.btn').addEventListener('click',darkmode)
+
+function onload() {
+    document.body.classList.toggle('dark-mode',localStorage.getItem('darkmode') === 'true')
+}
+document.addEventListener('DOMContentLoaded', onload)
