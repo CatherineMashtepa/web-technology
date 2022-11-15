4015bf53f0fc382b3a66092e90e40ea55dd8565a
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-11-15 18:47:37 +0300

    конечный почти

--- 1-3 wb/main.js
+++ 1-3 wb/main.js
@@ -1,14 +1,8 @@
 function darkmode () {
     const body = document.body
     const wasDarkMode = localStorage.getItem('darkmode') === 'true'
-
+    if (!wasDarkMode) { document.getElementById("toggle-theme-image").src = "sun.png"; }
+    else { document.getElementById("toggle-theme-image").src = "moon.png"; }
     localStorage.setItem('darkmode',!wasDarkMode)
     body.classList.toggle('dark-mode',!wasDarkMode)
 }
-
-document.querySelector('.btn').addEventListener('click',darkmode)
-
-function onload() {
-    document.body.classList.toggle('dark-mode',localStorage.getItem('darkmode') === 'true')
-}
-document.addEventListener('DOMContentLoaded', onload)
