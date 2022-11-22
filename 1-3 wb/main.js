c91092bb622c06db45be8442f1b98eb19696d3d8
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-11-22 12:23:11 +0300

    правки

--- 1-3 wb/main.js
+++ 1-3 wb/main.js
@@ -1,8 +1,12 @@
 function darkmode () {
     const body = document.body
     const wasDarkMode = localStorage.getItem('darkmode') === 'true'
+    // Выбираем настройки темы из localStorage
     if (!wasDarkMode) { document.getElementById("toggle-theme-image").src = "sun.png"; }
+    // Если текущая тема в localStorage равна "dark" 
     else { document.getElementById("toggle-theme-image").src = "moon.png"; }
+     // …тогда мы используем класс .darkmode
     localStorage.setItem('darkmode',!wasDarkMode)
+    // После чего сохраняем выбор в localStorage
     body.classList.toggle('dark-mode',!wasDarkMode)
 }
