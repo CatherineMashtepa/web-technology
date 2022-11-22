efd441139fcd92526315fc62ba02e06c366ef84d
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-11-23 01:12:14 +0300

    работает

--- 1-3 wb/main.js
+++ 1-3 wb/main.js
@@ -1,12 +1,23 @@
+const body = document.body
+var darkMode = localStorage.getItem('darkmode') == 'true';
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    changeMode(darkMode)
+});
+
 function darkmode () {
-    const body = document.body
-    const wasDarkMode = localStorage.getItem('darkmode') === 'true'
-    // Выбираем настройки темы из localStorage
-    if (!wasDarkMode) { document.getElementById("toggle-theme-image").src = "sun.png"; }
-    // Если текущая тема в localStorage равна "dark" 
-    else { document.getElementById("toggle-theme-image").src = "moon.png"; }
-     // …тогда мы используем класс .darkmode
-    localStorage.setItem('darkmode',!wasDarkMode)
-    // После чего сохраняем выбор в localStorage
-    body.classList.toggle('dark-mode',!wasDarkMode)
+    changeMode(!darkMode)
+}
+
+function changeMode (dark) {
+    darkMode = dark;
+    localStorage.setItem('darkmode', dark)
+    body.classList.toggle('dark-mode', dark)
+    if (!dark) 
+    { 
+        document.getElementById("toggle-theme-image").src = "sun.png"; 
+    }
+    else { 
+        document.getElementById("toggle-theme-image").src = "moon.png"; 
+    }
 }
